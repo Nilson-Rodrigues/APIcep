c7dbccf2bb68b9538f507ece647cb95e9a28a73a
Francinilson Rodrigues <58186025+Nilson-Rodrigues@users.noreply.github.com>
2023-02-19 18:22:34 -0300

    fist commit

---
+++ src/js/script_buscar_cep.js
@@ -0,0 +1,41 @@
+const btnBuscar = document.querySelector('#btnBuscar');
+const inputCep = document.querySelector('#searchCep');
+
+
+function getAdress() {
+    const cep = inputCep.value.replace("-","");
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    fetch(url).then(Response => Response.json())
+        .then(data => {
+            const dados = data;
+            const
+                cidade = document.querySelector('#cidade'),
+                rua = document.querySelector('#rua'),
+                complemento = document.querySelector('#complemento'),
+                bairro = document.querySelector('#bairro'),
+                estado = document.querySelector('#estado'),
+                ibge = document.querySelector('#ibge');
+
+
+            cidade.value = dados.localidade;
+            rua.value = dados.logradouro;
+            complemento.value = dados.complemento;
+            bairro.value = dados.bairro;
+            estado.value = dados.uf;
+            ibge.value = dados.ibge;
+        }).catch(error => {
+            console.log(error);
+            inputCep.value = '';
+            alert("Cep não encontrado");
+        })
+}
+btnBuscar.addEventListener("click", () => {
+    getAdress();
+})
+inputCep.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" || e.keyCode === 13) {
+        e.preventDefault();
+        getAdress();
+    }
+})
